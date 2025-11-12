2081835b155bcdc775d2026a68f190bff790095b
meankit025 <70406910+meankit025@users.noreply.github.com>
2025-11-12 18:04:39 +0530

    stream and sentry integration

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,17 +5,27 @@ import { connectDB } from './config/db.js';
 import { clerkMiddleware } from '@clerk/express';
 import { serve } from 'inngest/express';
 import { inngest, functions } from './config/inngest.js';
+import chatRoutes from './routes/chat.routes.js';
+import '../instrument.mjs';
+import * as Sentry from '@sentry/node';
 
 const app = express();
 
 app.use(express.json());
-app.use(clerkMiddleware()); // req.auth will now be available in req.auth object
-
-app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use(clerkMiddleware()); // req.auth will now be available in req object
 
 app.get('/', (req, res) => {
   res.send('API is running....');
 });
+
+app.get('/debug-sentry', function mainHandler(req, res) {
+  throw new Error('My first Sentry error!');
+});
+
+app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use('/api/chat', chatRoutes);
+
+Sentry.setupExpressErrorHandler(app);
 
 const serverStart = async () => {
   try {
